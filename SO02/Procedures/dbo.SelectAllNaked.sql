ef57cc08581fae63e733f8b43e28bfd088626929
zoxxx23 <77357748+zoxxx23@users.noreply.github.com>
2021-01-28 11:02:15 +0100

    Update dbo.SelectAllNaked.sql

--- SO02/Procedures/dbo.SelectAllNaked.sql
+++ SO02/Procedures/dbo.SelectAllNaked.sql
@@ -1,4 +1,9 @@
-CREATE PROCEDURE SelectAllNaked
-AS
-SELECT * FROM Motorcycles WHERE Type = 'Naked'
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+/****** Object:  StoredProcedure [dbo].[SelectAllNaked]    Script Date: 1/28/2021 9:53:21 AM ******/
+
+CREATE PROCEDURE [dbo].[SelectAllNaked]
+as
+SELECT * FROM Motorcycles WHERE Type='Naked'
 GO

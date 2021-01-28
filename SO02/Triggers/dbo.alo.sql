f6c6e10966c3bdd21fd45798fd97cb73a379125a
zoxxx23 <77357748+zoxxx23@users.noreply.github.com>
2021-01-28 09:04:51 +0100

    Update dbo.alo.sql

--- SO02/Triggers/dbo.alo.sql
+++ SO02/Triggers/dbo.alo.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TRIGGER [alo]
+create TRIGGER [alo]
 ON [dbo].[EnableDisableTrigger]
 AFTER UPDATE   
 AS RAISERROR ('Notify Customer Relations', 16, 10);

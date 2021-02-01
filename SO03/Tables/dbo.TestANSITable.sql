4c7ca772c873395e609a612abefc2066a170181b
zoxxx23 <77357748+zoxxx23@users.noreply.github.com>
2021-02-01 08:06:19 +0100

    Update dbo.TestANSITable.sql

--- SO03/Tables/dbo.TestANSITable.sql
+++ SO03/Tables/dbo.TestANSITable.sql
@@ -1,7 +1,3 @@
-SET ANSI_NULLS ON
-SET QUOTED_IDENTIFIER ON
-SET ANSI_PADDING ON
-GO
 CREATE TABLE [dbo].[TestANSITable] (
 		[1col]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[2col]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,

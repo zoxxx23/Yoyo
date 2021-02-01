760343af9adca3d2f78265bc36d10cd63623aa47
zoxxx23 <zoxxx23>
2021-01-31 22:46:38 -0800

    1

---
+++ SO03/Procedures/dbo.TestANSI.sql
@@ -0,0 +1,12 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE TestANSI
+AS
+SELECT * FROM TestCasing
+GO
---
+++ SO03/Tables/dbo.TestGrant.sql
@@ -0,0 +1,18 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[TestGrant] (
+		[ID]        [int] NOT NULL,
+		[Brand]     [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Model]     [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Type]      [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		CONSTRAINT [PK_TestGrant]
+		PRIMARY KEY
+		CLUSTERED
+		([ID])
+	ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[TestGrant] SET (LOCK_ESCALATION = TABLE)
+GO

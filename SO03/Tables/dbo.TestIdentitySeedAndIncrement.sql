118597b433f62f6ccc42cf348c3d09c38a30de9e
zoxxx23 <zoxxx23>
2021-01-31 23:25:14 -0800

    5

---
+++ SO03/Tables/dbo.TestIdentitySeedAndIncrement.sql
@@ -0,0 +1,18 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[TestIdentitySeedAndIncrement] (
+		[PersonId]      [int] IDENTITY(1, 2) NOT NULL,
+		[LastName]      [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+		[FirstName]     [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Age]           [int] NULL,
+		CONSTRAINT [PK__TestIden__AA2FFBE5CEF7E596]
+		PRIMARY KEY
+		CLUSTERED
+		([PersonId])
+	ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[TestIdentitySeedAndIncrement] SET (LOCK_ESCALATION = TABLE)
+GO

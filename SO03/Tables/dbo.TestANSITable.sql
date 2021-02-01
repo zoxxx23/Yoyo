bd98ddfcbbeed4229c722b9d5234e4c509f0e8f2
zoxxx23 <zoxxx23>
2021-01-31 23:05:56 -0800

    3

---
+++ SO03/Tables/dbo.TestANSITable.sql
@@ -0,0 +1,12 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[TestANSITable] (
+		[1col]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[2col]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[3col]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[TestANSITable] SET (LOCK_ESCALATION = TABLE)
+GO

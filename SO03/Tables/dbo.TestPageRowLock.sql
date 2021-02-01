550911c273cca02565fe7a46e971860e3a47195b
zoxxx23 <77357748+zoxxx23@users.noreply.github.com>
2021-02-01 08:35:07 +0100

    Update dbo.TestPageRowLock.sql

--- SO03/Tables/dbo.TestPageRowLock.sql
+++ SO03/Tables/dbo.TestPageRowLock.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
-CREATE TABLE [dbo].[TestPageRowLOCK] (
+CREATE TABLE [dbo].[TestPageRowLock] (
 		[PersonId]      [int] NOT NULL,
 		[LastName]      [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 		[FirstName]     [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,

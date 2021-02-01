4d4dd9d57834f79577b4b520efa8f343adb4c4bb
zoxxx23 <zoxxx23>
2021-01-31 23:21:58 -0800

    4

---
+++ SO03/Tables/dbo.TestFillfactor.sql
@@ -0,0 +1,23 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[TestFillfactor] (
+		[PersonId]      [int] NOT NULL,
+		[LastName]      [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+		[FirstName]     [varchar](255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Age]           [int] NULL,
+		CONSTRAINT [PK__TestFill__AA2FFBE5CDA3FB5A]
+		PRIMARY KEY
+		CLUSTERED
+		([PersonId])
+	ON [PRIMARY]
+) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [IndexBRE]
+	ON [dbo].[TestFillfactor] ([PersonId])
+	WITH ( FILLFACTOR = 5)
+	ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[TestFillfactor] SET (LOCK_ESCALATION = TABLE)
+GO

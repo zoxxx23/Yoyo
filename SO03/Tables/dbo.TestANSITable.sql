3a76a376d66ad00ad6fe8d09917850ee2f25081d
zoxxx23 <77357748+zoxxx23@users.noreply.github.com>
2021-02-03 14:46:07 +0100

    Update dbo.TestANSITable.sql

--- SO03/Tables/dbo.TestANSITable.sql
+++ SO03/Tables/dbo.TestANSITable.sql
@@ -1,8 +1,16 @@
+USE [TESTBre]
+GO
+
+/****** Object:  Table [dbo].[TestANSITable]    Script Date: 2/3/2021 2:45:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
 CREATE TABLE [dbo].[TestANSITable] (
 		[1col]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[2col]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[3col]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[TestANSITable] SET (LOCK_ESCALATION = TABLE)
 GO

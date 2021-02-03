831572fb45e431e96fe5c9474cc5b33d936bdc0c
zoxxx23 <77357748+zoxxx23@users.noreply.github.com>
2021-02-03 14:47:28 +0100

    Update dbo.TestANSITable.sql

--- SO03/Tables/dbo.TestANSITable.sql
+++ SO03/Tables/dbo.TestANSITable.sql
@@ -1,13 +1,6 @@
-USE [TESTBre]
-GO
-
-/****** Object:  Table [dbo].[TestANSITable]    Script Date: 2/3/2021 2:45:27 PM ******/
 SET ANSI_NULLS ON
-GO
-
 SET QUOTED_IDENTIFIER OFF
 GO
-
 CREATE TABLE [dbo].[TestANSITable] (
 		[1col]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[2col]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,

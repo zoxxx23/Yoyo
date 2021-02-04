c7e664bfdb0a88ca3e997baf44577743ae5f5823
zoxxx23 <77357748+zoxxx23@users.noreply.github.com>
2021-02-04 11:55:44 +0100

    Update Person.Person.sql

--- Bug201634/Tables/Person.Person.sql
+++ Bug201634/Tables/Person.Person.sql
@@ -16,6 +16,7 @@ CREATE TABLE [Person].[Person] (
 		[Demographics]              [xml](CONTENT [Person].[IndividualSurveySchemaCollection]) NULL,
 		[rowguid]                   [uniqueidentifier] NOT NULL ROWGUIDCOL,
 		[ModifiedDate]              [datetime] NOT NULL,
+		[Hahahahahaha]              [datetime] NULL,
 		CONSTRAINT [PK_Person_BusinessEntityID]
 		PRIMARY KEY
 		CLUSTERED

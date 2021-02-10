8500f15c3c2ebefff9fa2708163da9532789d0b1
zoxxx23 <zoxxx23>
2021-02-10 07:07:56 -0800

    W

--- B/Tables/dbo.DatabaseLog.sql
+++ B/Tables/dbo.DatabaseLog.sql
@@ -11,6 +11,7 @@ CREATE TABLE [dbo].[DatabaseLog] (
 		[Object]            [sysname] COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[TSQL]              [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 		[XmlEvent]          [xml] NOT NULL,
+		[NEW]               [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		CONSTRAINT [PK_DatabaseLog_DatabaseLogID]
 		PRIMARY KEY
 		NONCLUSTERED

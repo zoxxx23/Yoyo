86f4128e67ee4c8c24fba877685921784bf4d7a4
zoxxx23 <zoxxx23>
2021-02-11 01:24:31 -0800

    2 - static

---
+++ C/Tables/StaticData/HumanResources.JobCandidate [Data].sql
@@ -0,0 +1,15 @@
+SET IDENTITY_INSERT [HumanResources].[JobCandidate] ON
+INSERT INTO [HumanResources].[JobCandidate] ([JobCandidateID], [BusinessEntityID], [ModifiedDate]) VALUES (1, NULL, '20070623 00:00:00.000')
+INSERT INTO [HumanResources].[JobCandidate] ([JobCandidateID], [BusinessEntityID], [ModifiedDate]) VALUES (2, NULL, '20070623 00:00:00.000')
+INSERT INTO [HumanResources].[JobCandidate] ([JobCandidateID], [BusinessEntityID], [ModifiedDate]) VALUES (3, NULL, '20070623 00:00:00.000')
+INSERT INTO [HumanResources].[JobCandidate] ([JobCandidateID], [BusinessEntityID], [ModifiedDate]) VALUES (4, 274, '20131222 18:32:21.313')
+INSERT INTO [HumanResources].[JobCandidate] ([JobCandidateID], [BusinessEntityID], [ModifiedDate]) VALUES (5, NULL, '20070623 00:00:00.000')
+INSERT INTO [HumanResources].[JobCandidate] ([JobCandidateID], [BusinessEntityID], [ModifiedDate]) VALUES (6, NULL, '20070623 00:00:00.000')
+INSERT INTO [HumanResources].[JobCandidate] ([JobCandidateID], [BusinessEntityID], [ModifiedDate]) VALUES (7, NULL, '20070623 00:00:00.000')
+INSERT INTO [HumanResources].[JobCandidate] ([JobCandidateID], [BusinessEntityID], [ModifiedDate]) VALUES (8, 212, '20131222 18:32:21.267')
+INSERT INTO [HumanResources].[JobCandidate] ([JobCandidateID], [BusinessEntityID], [ModifiedDate]) VALUES (9, NULL, '20070623 00:00:00.000')
+INSERT INTO [HumanResources].[JobCandidate] ([JobCandidateID], [BusinessEntityID], [ModifiedDate]) VALUES (10, NULL, '20070623 00:00:00.000')
+INSERT INTO [HumanResources].[JobCandidate] ([JobCandidateID], [BusinessEntityID], [ModifiedDate]) VALUES (11, NULL, '20070623 00:00:00.000')
+INSERT INTO [HumanResources].[JobCandidate] ([JobCandidateID], [BusinessEntityID], [ModifiedDate]) VALUES (12, NULL, '20070623 00:00:00.000')
+INSERT INTO [HumanResources].[JobCandidate] ([JobCandidateID], [BusinessEntityID], [ModifiedDate]) VALUES (13, NULL, '20070623 00:00:00.000')
+SET IDENTITY_INSERT [HumanResources].[JobCandidate] OFF

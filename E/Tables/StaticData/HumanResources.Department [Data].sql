50d8f3f85532e746cc9d9c7155ce151ad0925b98
zoxxx23 <zoxxx23>
2021-02-12 03:07:31 -0800

    - static data 1st time

---
+++ E/Tables/StaticData/HumanResources.Department [Data].sql
@@ -0,0 +1,18 @@
+SET IDENTITY_INSERT [HumanResources].[Department] ON
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (1, N'Engineering', N'Research and Development', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (2, N'Tool Design', N'Research and Development', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (3, N'Sales', N'Sales and Marketing', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (4, N'Marketing', N'Sales and Marketing', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (5, N'Purchasing', N'Inventory Management', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (6, N'Research and Development', N'Research and Development', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (7, N'Production', N'Manufacturing', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (8, N'Production Control', N'Manufacturing', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (9, N'Human Resources', N'Executive General and Administration', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (10, N'Finance', N'Executive General and Administration', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (11, N'Information Services', N'Executive General and Administration', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (12, N'Document Control', N'Quality Assurance', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (13, N'Quality Assurance', N'Quality Assurance', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (14, N'Facilities and Maintenance', N'Executive General and Administration', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (15, N'Shipping and Receiving', N'Inventory Management', '20080430 00:00:00.000', NULL)
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate], [OBoze]) VALUES (16, N'Executive', N'Executive General and Administration', '20080430 00:00:00.000', NULL)
+SET IDENTITY_INSERT [HumanResources].[Department] OFF

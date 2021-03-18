9ce68a1aaf1a14e13ba7ed53937c4c4fe01bd219
zoxxx23 <zoxxx23>
2021-03-18 15:10:10 +0100

    novo novo

--- TESTIRAMObre/Tables/dbo.aux.sql
+++ TESTIRAMObre/Tables/dbo.aux.sql
@@ -5,7 +5,8 @@ GO
 CREATE TABLE [dbo].[aux] (
 		[ww]        [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[NOVO]      [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[NOVO2]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[NOVO2]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[NOVO3]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[aux] SET (LOCK_ESCALATION = TABLE)

c38c44385920f2dd81be61b85691069537d846a0
zoxxx23 <zoxxx23>
2021-04-01 04:29:10 -0700

    rrr

---
+++ SD2019D/Tables/StaticData/HumanResources.Department [Data].sql
@@ -0,0 +1,18 @@
+SET IDENTITY_INSERT [HumanResources].[Department] ON
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (1, N'Engineering', N'Research and Development', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (2, N'Tool Design', N'Research and Development', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (3, N'Sales', N'Sales and Marketing', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (4, N'Marketing', N'Sales and Marketing', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (5, N'Purchasing', N'Inventory Management', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (6, N'Research and Development', N'Research and Development', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (7, N'Production', N'Manufacturing', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (8, N'Production Control', N'Manufacturing', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (9, N'Human Resources', N'Executive General and Administration', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (10, N'Finance', N'Executive General and Administration', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (11, N'Information Services', N'Executive General and Administration', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (12, N'Document Control', N'Quality Assurance', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (13, N'Quality Assurance', N'Quality Assurance', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (14, N'Facilities and Maintenance', N'Executive General and Administration', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (15, N'Shipping and Receiving', N'Inventory Management', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Department] ([DepartmentID], [Name], [GroupName], [ModifiedDate]) VALUES (16, N'Executive', N'Executive General and Administration', '20080430 00:00:00.000')
+SET IDENTITY_INSERT [HumanResources].[Department] OFF
---
+++ SD2019D/Tables/StaticData/HumanResources.Shift [Data].sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [HumanResources].[Shift] ON
+INSERT INTO [HumanResources].[Shift] ([ShiftID], [Name], [StartTime], [EndTime], [ModifiedDate]) VALUES (1, N'Day', '07:00:00', '15:00:00', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Shift] ([ShiftID], [Name], [StartTime], [EndTime], [ModifiedDate]) VALUES (2, N'Evening', '15:00:00', '23:00:00', '20080430 00:00:00.000')
+INSERT INTO [HumanResources].[Shift] ([ShiftID], [Name], [StartTime], [EndTime], [ModifiedDate]) VALUES (3, N'Night', '23:00:00', '07:00:00', '20080430 00:00:00.000')
+SET IDENTITY_INSERT [HumanResources].[Shift] OFF

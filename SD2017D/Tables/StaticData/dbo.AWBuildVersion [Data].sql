45050efaa5f9745daa752944967d967b345860cb
zoxxx23 <77357748+zoxxx23@users.noreply.github.com>
2021-04-01 10:38:17 +0200

    Update dbo.AWBuildVersion [Data].sql

--- SD2017D/Tables/StaticData/dbo.AWBuildVersion [Data].sql
+++ SD2017D/Tables/StaticData/dbo.AWBuildVersion [Data].sql
@@ -1,3 +1,5 @@
 SET IDENTITY_INSERT [dbo].[AWBuildVersion] ON
 INSERT INTO [dbo].[AWBuildVersion] ([SystemInformationID], [Database Version], [VersionDate], [ModifiedDate]) VALUES (1, N'14.0.1000.169', '20170822 19:39:35.643', '20171027 14:33:02.347')
+INSERT INTO [dbo].[AWBuildVersion] ([SystemInformationID], [Database Version], [VersionDate], [ModifiedDate]) VALUES (2, N'14.0.1000.169', '20170822 19:39:35.643', '20171027 14:33:02.347')
+INSERT INTO [dbo].[AWBuildVersion] ([SystemInformationID], [Database Version], [VersionDate], [ModifiedDate]) VALUES (3, N'14.0.1000.169', '20170822 19:39:35.643', '20171027 14:33:02.347')
 SET IDENTITY_INSERT [dbo].[AWBuildVersion] OFF

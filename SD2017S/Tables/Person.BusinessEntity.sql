bc2afbe345db78ebb040b36374433594542c7520
zoxxx23 <zoxxx23>
2021-04-01 02:09:53 -0700

    init

---
+++ SD2017S/Tables/Person.BusinessEntity.sql
@@ -0,0 +1,49 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING OFF
+GO
+CREATE TABLE [Person].[BusinessEntity] (
+		[BusinessEntityID]     [int] IDENTITY(1, 1) NOT FOR REPLICATION NOT NULL,
+		[rowguid]              [uniqueidentifier] NOT NULL ROWGUIDCOL,
+		[ModifiedDate]         [datetime] NOT NULL,
+		CONSTRAINT [PK_BusinessEntity_BusinessEntityID]
+		PRIMARY KEY
+		CLUSTERED
+		([BusinessEntityID])
+	ON [PRIMARY]
+) ON [PRIMARY]
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Clustered index created by a primary key constraint.', 'SCHEMA', N'Person', 'TABLE', N'BusinessEntity', 'INDEX', N'PK_BusinessEntity_BusinessEntityID'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Primary key (clustered) constraint', 'SCHEMA', N'Person', 'TABLE', N'BusinessEntity', 'CONSTRAINT', N'PK_BusinessEntity_BusinessEntityID'
+GO
+ALTER TABLE [Person].[BusinessEntity]
+	ADD
+	CONSTRAINT [DF_BusinessEntity_rowguid]
+	DEFAULT (newid()) FOR [rowguid]
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Default constraint value of NEWID()', 'SCHEMA', N'Person', 'TABLE', N'BusinessEntity', 'CONSTRAINT', N'DF_BusinessEntity_rowguid'
+GO
+ALTER TABLE [Person].[BusinessEntity]
+	ADD
+	CONSTRAINT [DF_BusinessEntity_ModifiedDate]
+	DEFAULT (getdate()) FOR [ModifiedDate]
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Default constraint value of GETDATE()', 'SCHEMA', N'Person', 'TABLE', N'BusinessEntity', 'CONSTRAINT', N'DF_BusinessEntity_ModifiedDate'
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [AK_BusinessEntity_rowguid]
+	ON [Person].[BusinessEntity] ([rowguid])
+	ON [PRIMARY]
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Unique nonclustered index. Used to support replication samples.', 'SCHEMA', N'Person', 'TABLE', N'BusinessEntity', 'INDEX', N'AK_BusinessEntity_rowguid'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Source of the ID that connects vendors, customers, and employees with address and contact information.', 'SCHEMA', N'Person', 'TABLE', N'BusinessEntity', NULL, NULL
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Primary key for all customers, vendors, and employees.', 'SCHEMA', N'Person', 'TABLE', N'BusinessEntity', 'COLUMN', N'BusinessEntityID'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'ROWGUIDCOL number uniquely identifying the record. Used to support a merge replication sample.', 'SCHEMA', N'Person', 'TABLE', N'BusinessEntity', 'COLUMN', N'rowguid'
+GO
+EXEC sp_addextendedproperty N'MS_Description', N'Date and time the record was last updated.', 'SCHEMA', N'Person', 'TABLE', N'BusinessEntity', 'COLUMN', N'ModifiedDate'
+GO
+ALTER TABLE [Person].[BusinessEntity] SET (LOCK_ESCALATION = TABLE)
+GO

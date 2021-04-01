5d7230fb8e64952b7e392506f4124fe105bc23da
zoxxx23 <zoxxx23>
2021-04-01 04:25:03 -0700

    i

---
+++ SD2019S/Tables/StaticData/dbo.AWBuildVersion [Data].sql
@@ -0,0 +1,3 @@
+SET IDENTITY_INSERT [dbo].[AWBuildVersion] ON
+INSERT INTO [dbo].[AWBuildVersion] ([SystemInformationID], [Database Version], [VersionDate], [ModifiedDate]) VALUES (1, N'14.0.1000.169', '20170822 19:39:35.643', '20171027 14:33:02.347')
+SET IDENTITY_INSERT [dbo].[AWBuildVersion] OFF

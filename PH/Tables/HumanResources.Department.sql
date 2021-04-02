6d29a23c7ddb891e9543390f2bf5b0659961549a
zoxxx23 <zoxxx23>
2021-04-01 23:29:02 -0700

    4. commit

--- PH/Tables/HumanResources.Department.sql
+++ PH/Tables/HumanResources.Department.sql
@@ -8,6 +8,8 @@ CREATE TABLE [HumanResources].[Department] (
 		[GroupName]        [dbo].[Name] NOT NULL,
 		[ModifiedDate]     [datetime] NOT NULL,
 		[r]                [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[rrr]              [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[ttt]              [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS SPARSE NULL,
 		CONSTRAINT [PK_Department_DepartmentID]
 		PRIMARY KEY
 		CLUSTERED

eb24e0630abc763e1cbb9d537cd5480179e80f68
zoxxx23 <77357748+zoxxx23@users.noreply.github.com>
2021-04-15 09:09:22 +0200

    Update Person.Person.sql

--- blblbl/Tables/Person.Person.sql
+++ blblbl/Tables/Person.Person.sql
@@ -10,7 +10,7 @@ CREATE TABLE [Person].[Person] (
 		[FirstName]                 [dbo].[Name] NOT NULL,
 		[MiddleName]                [dbo].[Name] NULL,
 		[LastName]                  [dbo].[Name] NOT NULL,
-		[Suffix]                    [nvarchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[SuffixZ]                    [nvarchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 		[EmailPromotion]            [int] NOT NULL,
 		[AdditionalContactInfo]     [xml](CONTENT [Person].[AdditionalContactInfoSchemaCollection]) NULL,
 		[Demographics]              [xml](CONTENT [Person].[IndividualSurveySchemaCollection]) NULL,
